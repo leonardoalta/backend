62ab01bc46b3a6a2b187807c339827e831941cfd
leonardoalta <leonardoalta7688@gmail.com>
2025-12-16 06:07:53 -0600

    weeks Dasboard

--- src/main/java/com/blacksystem/system/models/dto/UserProfileDTO.java
+++ src/main/java/com/blacksystem/system/models/dto/UserProfileDTO.java
@@ -1,23 +1,23 @@
 package com.blacksystem.system.models.dto;
 
 import com.blacksystem.system.models.User;
+public class UserProfileDTO {
 
-public record UserProfileDTO(
-        Long id,
-        String nameUser,
-        String lastNameUser,
-        String middleNameUser,
-        String emailUser,
-        String phoneUser
-) {
+    public Long idUser;
+    public String nameUser;
+    public String lastNameUser;
+    public String middleNameUser;
+    public String emailUser;
+    public String phoneUser;
+
     public static UserProfileDTO from(User user) {
-        return new UserProfileDTO(
-                user.getIdUser(),
-                user.getNameUser(),
-                user.getLastNameUser(),
-                user.getMiddleNameUser(),
-                user.getEmailUser(),
-                user.getPhoneUser()
-        );
+        UserProfileDTO dto = new UserProfileDTO();
+        dto.idUser = user.getIdUser();
+        dto.nameUser = user.getNameUser();
+        dto.lastNameUser = user.getLastNameUser();
+        dto.middleNameUser = user.getMiddleNameUser();
+        dto.emailUser = user.getEmailUser();
+        dto.phoneUser = user.getPhoneUser();
+        return dto;
     }
 }
