16395c3ba3c803ed397c4e8521b18f0d41762232
leonardoalta <leonardoalta7688@gmail.com>
2025-12-22 20:09:11 -0600

    Error Transactional

--- src/main/java/com/blacksystem/system/services/PetPhysicalService.java
+++ src/main/java/com/blacksystem/system/services/PetPhysicalService.java
@@ -7,9 +7,12 @@ import com.blacksystem.system.models.enums.BodyConditionScore;
 import com.blacksystem.system.repositorys.PetPhysicalRepository;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.List;
 @Service
+@Transactional
+
 public class PetPhysicalService {
 
     private final PetPhysicalRepository repository;
@@ -18,25 +21,26 @@ public class PetPhysicalService {
         this.repository = repository;
     }
 
+    @Transactional
     public PetPhysicalData save(Pet pet, PhysicalDataRequest req) {
+
+        System.out.println("🔥 GUARDANDO CONTROL FÍSICO PARA PET ID: " + pet.getId());
+        System.out.println("BCS = " + req.getBcs());
 
         PetPhysicalData data = new PetPhysicalData();
         data.setPet(pet);
 
-        // ⚖️ peso base
         data.setWeightKg(
                 req.getWeightKg() != null ? req.getWeightKg() : pet.getWeight()
         );
 
         data.setHeightCm(req.getHeightCm());
-
-        // 🔥 AQUÍ YA FUNCIONA
         data.setBcs(BodyConditionScore.fromValue(req.getBcs()));
-
         data.setRecordedAt(LocalDate.parse(req.getRecordedAt()));
 
         return repository.save(data);
     }
+
 
     public List<PetPhysicalData> getHistory(Pet pet) {
         return repository.findByPetOrderByRecordedAtDesc(pet);
